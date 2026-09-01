583dd8dd1e3f95528b8d468d7a78bc1b790ad924
ASHISH KUMAR PAL <106040189+Ashish-kumar-pal-1436@users.noreply.github.com>
2026-09-02 01:29:03 +0530

    Add Home and Login components with routing support

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,16 @@
 
 import {Routes, Route} from 'react-router-dom'
+import Home from './pages/Home'
+import Login from './pages/Login'
 const App = () => {
   return (
     <>
       <Routes>
-         <Route />
+         <Route  path='/' element={ <Home />} />
+         <Route  path= '/login' element={ <Login />} />
       </Routes> 
       <div>
-        
+
       </div>
     </>
   )
