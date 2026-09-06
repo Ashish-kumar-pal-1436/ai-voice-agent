b4c5e7ab5e8b582f61b54544cbab867053c2b82c
ASHISH KUMAR PAL <106040189+Ashish-kumar-pal-1436@users.noreply.github.com>
2026-09-06 14:28:51 +0530

    feat: enhance askAssistant function with user validation and navigation handling

--- backend/controllers/assistant.controller.js
+++ backend/controllers/assistant.controller.js
@@ -16,7 +16,93 @@ export const getAssistantConfig = async (req, res) =>{
 
 export const askAssistant = async (req, res) => {
     try {
-        const {message, userId} = req.body
+        const {message, userId} = req.body 
+        if(!message || userId){
+            return res.status(400).json({message:"Message and userId are required"})
+        }
+
+        const user = await User.findById(userId) 
+
+        if(!user) {
+             return res.status(404).json({message: "User is not found"})
+        } 
+
+        if(!user.geminiApiKey){
+            return res.status(404).json({message:"gemini apikey is not added"})
+        }
+
+        if(user.plan === "free" && user.totalMessages >= user.requestLimit){
+            return res.status(400).json({message: "Free limit reached"})
+        } 
+
+        if(user.plan === "pro" && new Date(user.proExpiresAt) < new Date()){
+            user.plan === "free"
+            await user.save()
+            return res.status(400).json({message: "Pro plan expired"})
+        }
+
+        const cleanMessage = message.toLowerCase() 
+
+        if(user.enableNavigation){
+
+            // Navigation Commands
+            const navigationWords = [
+                "open",
+                "go",
+                "start",
+                "show",
+                "navigate",
+                "task me"
+            ]
+
+            //Check navigation intent
+            const wantsNavigation =
+             navigationWords.some((word) =>
+                cleanMessage.startWith(word)
+            )
+
+            //User wants navigation
+
+            if(wantsNavigation) {
+
+                //Find matching page
+                const matchedPage =
+                user.pages.find((page) =>
+                    page.keywords.some((keyword) =>
+                        cleanMessage.includes(
+                            keyword.toLowerCase()
+                        )
+                    )
+                )
+
+                // Page found
+                if (matchedPage) {
+
+                    //Already open
+                    if (
+                        req.body.currentPath === 
+                        matchedPage.path
+                    ) {
+                        return res.json({
+                            success: true,
+                            response:
+                            `${matchedPage.name} already open`
+                        })
+                    }
+
+                    // Navigate
+
+                    return res.json({
+                        success: true,
+                        action: "navigate",
+                        path: matchedPage.path,
+                        response:
+                        `Opening ${matchedPage.name}`
+                    })
+
+                }
+            }
+        }
     } catch (error) {
         
     }
